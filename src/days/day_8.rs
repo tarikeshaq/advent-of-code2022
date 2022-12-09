ce30c5030ea365dd2c9fd78e31956e407e569f2d
Tarik Eshaq <teshaq@mozilla.com>
2022-12-09 01:40:40 -0500

    Adds day 8 and 9

--- src/days/day_8.rs
+++ src/days/day_8.rs
@@ -1,13 +1,142 @@
+use std::{
+    collections::{HashMap, HashSet},
+    vec,
+};
+
 use super::DaySolver;
 
 pub struct Solver;
 
+fn make_grid(input_txt: &str) -> Vec<Vec<usize>> {
+    let mut res = Vec::new();
+    for line in input_txt.lines() {
+        res.push(
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect(),
+        )
+    }
+    res
+}
+
 impl DaySolver for Solver {
-    fn q2(&self, _input_txt: &str) -> String {
-        todo!()
+    fn q2(&self, input_txt: &str) -> String {
+        let grid: Vec<Vec<usize>> = make_grid(input_txt);
+        let m = grid[0].len();
+        let n = grid.len();
+        let mut results = HashMap::new();
+        for i in 0..n {
+            for j in 0..m {
+                let item = grid[i][j] as i32;
+                let mut res = 0;
+                for k in (0..j).rev() {
+                    res += 1;
+                    let other = grid[i][k] as i32;
+                    if other >= item {
+                        break;
+                    }
+                }
+                results.entry((i, j)).or_insert(vec![1]).push(res);
+            }
+        }
+        for i in 0..n {
+            for j in (0..m).rev() {
+                let item = grid[i][j] as i32;
+                let mut res = 0;
+                for k in j + 1..m {
+                    res += 1;
+                    let other = grid[i][k] as i32;
+                    if other >= item {
+                        break;
+                    }
+                }
+                results.entry((i, j)).or_insert(vec![1]).push(res);
+            }
+        }
+
+        for j in 0..m {
+            for i in 0..n {
+                let item = grid[i][j] as i32;
+                let mut res = 0;
+                for k in (0..i).rev() {
+                    res += 1;
+                    let other = grid[k][j] as i32;
+                    if other >= item {
+                        break;
+                    }
+                }
+                results.entry((i, j)).or_insert(vec![1]).push(res);
+            }
+        }
+        for j in 0..m {
+            for i in (0..n).rev() {
+                let item = grid[i][j] as i32;
+                let mut res = 0;
+                for k in i + 1..n {
+                    res += 1;
+                    let other = grid[k][j] as i32;
+                    if other >= item {
+                        break;
+                    }
+                }
+                results.entry((i, j)).or_insert(vec![1]).push(res);
+            }
+        }
+        results
+            .iter()
+            .filter(|(key, _)| key.0 != 0 && key.0 != n - 1 && key.1 != 0 && key.1 != m - 1)
+            .map(|(_, scores)| scores.iter().fold(1, |acc, v| acc * v))
+            .max()
+            .unwrap()
+            .to_string()
     }
 
-    fn q1(&self, _input_txt: &str) -> String {
-        todo!()
+    fn q1(&self, input_txt: &str) -> String {
+        let grid: Vec<Vec<usize>> = make_grid(input_txt);
+        let m = grid[0].len();
+        let n = grid.len();
+        let mut seen = HashSet::new();
+        for i in 0..n {
+            let mut curr_size = -1;
+            for j in 0..m {
+                let item = grid[i][j] as i32;
+                if item > curr_size {
+                    curr_size = item;
+                    seen.insert((i, j));
+                }
+            }
+        }
+        for i in 0..n {
+            let mut curr_size = -1;
+            for j in (0..m).rev() {
+                let item = grid[i][j];
+                if item as i32 > curr_size {
+                    curr_size = item as i32;
+                    seen.insert((i, j));
+                }
+            }
+        }
+
+        for j in 0..m {
+            let mut curr_size = -1;
+            for i in 0..n {
+                let item = grid[i][j];
+                if item as i32 > curr_size {
+                    curr_size = item as i32;
+                    seen.insert((i, j));
+                }
+            }
+        }
+        for j in 0..m {
+            let mut curr_size = -1;
+            for i in (0..n).rev() {
+                let item = grid[i][j];
+                if item as i32 > curr_size {
+                    curr_size = item as i32;
+                    seen.insert((i, j));
+                }
+            }
+        }
+        seen.len().to_string()
     }
 }
--- src/days/day_9.rs
+++ src/days/day_9.rs
@@ -1,13 +1,156 @@
+use std::collections::HashSet;
+
 use super::DaySolver;
 
+
+enum Move {
+    Up,
+    Left,
+    Right,
+    Down
+}
+
+#[derive(Default)]
+struct State {
+    head: (i32, i32),
+    tail: (i32, i32)
+}
+
+#[derive(Default, Debug)]
+struct StateV2 {
+    states: [(i32, i32); 10]
+}
+
+
+impl Move {
+    fn new(s: &str) -> Self {
+        match s {
+            "R" => Move::Right,
+            "L" => Move::Left,
+            "U" => Move::Up,
+            "D" => Move::Down,
+            _ => panic!("OOF")
+        }
+    }
+}
+
+fn process_move2(states: &mut StateV2,  m: Move, amount: i32, res: &mut HashSet<(i32, i32)>) {
+    // first we blindly move the head state
+    for _ in 0..amount {
+        match m {
+            Move::Right => states.states[0].0 += 1,
+            Move::Left => states.states[0].0 -= 1,
+            Move::Down => states.states[0].1 += 1,
+            Move::Up => states.states[0].1 -= 1
+        }
+
+        let mut prev = states.states[0];
+        for i in 1..10 {
+            // now lets reconcile the tail's position
+            // If the distance is one away, we do not move, and just return early
+
+            // if we are here, we are more than one step away!
+            // lets find out what direction we should go
+            // the following is if we should go up
+            match (states.states[i].0 - prev.0, states.states[i].1 - prev.1) {
+                (2, 0) => states.states[i].0 -=1,
+                (-2, 0) => states.states[i].0 +=1,
+                (0, 2) => states.states[i].1 -=1,
+                (0, -2) => states.states[i].1 +=1,
+                (2, 1) | (1, 2) | (2, 2) => {
+                    states.states[i].0 -= 1;
+                    states.states[i].1 -= 1;
+                },
+                (-2, -1) | (-1, -2) | (-2, -2) => {
+                    states.states[i].0 += 1;
+                    states.states[i].1 += 1;
+                }
+                (-2, 1) | (-1, 2) | (-2, 2) => {
+                    states.states[i].0 += 1;
+                    states.states[i].1 -= 1;
+                },
+                (1, -2) | (2, -1) | (2, -2) => {
+                    states.states[i].0 -= 1;
+                    states.states[i].1 += 1;
+                },
+                _ => ()
+            }
+            prev = states.states[i];
+        }
+        res.insert((prev.0, prev.1));
+    }
+
+}
+
+fn process_move(state: &mut State,  m: Move, amount: i32, res: &mut HashSet<(i32, i32)>) {
+    // first we blindly move the head state
+    for _ in 0..amount {
+        match m {
+            Move::Right => state.head.0 += 1,
+            Move::Left => state.head.0 -= 1,
+            Move::Down => state.head.1 += 1,
+            Move::Up => state.head.1 -= 1
+        }
+
+        // now lets reconcile the tail's position
+        // If the distance is one away, we do not move, and just return early
+
+        // if we are here, we are more than one step away!
+        // lets find out what direction we should go
+        // the following is if we should go up
+         match (state.tail.0 - state.head.0, state.tail.1 - state.head.1) {
+            (2, 0) => state.tail.0 -=1,
+            (-2, 0) => state.tail.0 +=1,
+            (0, 2) => state.tail.1 -=1,
+            (0, -2) => state.tail.1 +=1,
+            (2, 1) | (1, 2) => {
+                state.tail.0 -= 1;
+                state.tail.1 -= 1;
+            },
+            (-2, -1) | (-1, -2) => {
+                state.tail.0 += 1;
+                state.tail.1 += 1;
+            }
+            (-2, 1) | (-1, 2) => {
+                state.tail.0 += 1;
+                state.tail.1 -= 1;
+            },
+            (1, -2) | (2, -1) => {
+                state.tail.0 -= 1;
+                state.tail.1 += 1;
+            }
+            (_, _) => ()
+         }
+        res.insert((state.tail.0, state.tail.1));
+    }
+
+}
 pub struct Solver;
 
 impl DaySolver for Solver {
-    fn q2(&self, _input_txt: &str) -> String {
-        todo!()
+    fn q2(&self, input_txt: &str) -> String {
+        let mut res = HashSet::new();
+        res.insert((0, 0));
+        let mut state = StateV2::default();
+        for line in input_txt.lines() {
+            let mut split = line.split_whitespace();
+            let m = Move::new(split.next().unwrap());
+            let amount = split.next().unwrap().parse::<i32>().unwrap();
+            process_move2(&mut state,  m, amount, &mut res);
+        }
+        res.len().to_string()
     }
 
-    fn q1(&self, _input_txt: &str) -> String {
-        todo!()
+    fn q1(&self, input_txt: &str) -> String {
+        let mut res = HashSet::new();
+        res.insert((0, 0));
+        let mut state = State::default();
+        for line in input_txt.lines() {
+            let mut split = line.split_whitespace();
+            let m = Move::new(split.next().unwrap());
+            let amount = split.next().unwrap().parse::<i32>().unwrap();
+            process_move(&mut state,  m, amount, &mut res);
+        }
+        res.len().to_string()
     }
 }
